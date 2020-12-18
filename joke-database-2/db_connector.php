c2b59c6cf6851eadbf50dc8a0c6892d6b5253805
ajohns5 <54555376+ajohns5@users.noreply.github.com>
2020-12-18 00:15:25 -0700

    Update db_connector.php

--- joke-database-2/db_connector.php
+++ joke-database-2/db_connector.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "joke-database-2";
+$servername = "localhost";
 $username = "lua70uj935zu8uy6";
 $password = "t0ycwbjc36tdlaps";
-$dbname = "jokes-database";
+$dbname = "joke-database-2";
 
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
