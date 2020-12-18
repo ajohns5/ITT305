1e1e0f5f53729d0f960163cee7a4ec1a78e544a0
ajohns5 <54555376+ajohns5@users.noreply.github.com>
2020-12-18 00:02:28 -0700

    Update db_connector.php

--- joke-database-2/db_connector.php
+++ joke-database-2/db_connector.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
+$servername = "jokes-database";
 $username = "lua70uj935zu8uy6";
 $password = "t0ycwbjc36tdlaps";
 $dbname = "qksehlwgd3n08qf3";
